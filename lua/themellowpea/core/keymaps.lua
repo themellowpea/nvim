f49f6bdba0def0d300ae5d2370632f322bf7ae25
Matt <themlp@icloud.com>
2024-09-20 10:35:15 -0400

    clean up keymaps and use icons for which-key

--- lua/themellowpea/core/keymaps.lua
+++ lua/themellowpea/core/keymaps.lua
@@ -12,7 +12,7 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>no", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x', { desc = "Delete w/o copying into register" })
@@ -37,10 +37,7 @@ keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer
 keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
 keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode" })
 
--- NOTE: NvChad Related Mappings
 local fn = vim.fn
-local cwd = vim.fn.stdpath "config" .. "/"
-local config_dir = { cwd }
 
 -- Remove All Text
 vim.keymap.set("n", "<leader>R", "<cmd>%d+<cr>", { desc = "General | Remove All Text", silent = true })
@@ -56,90 +53,64 @@ vim.keymap.set("n", "<leader>c", "<cmd>Bdelete!<cr>", { desc = "General | Close 
 
 -- Toggle Tabufline
 vim.keymap.set("n", "<leader>ob", function()
-  if vim.o.showtabline == 2 then
-    vim.o.showtabline = 0
-    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Tabufline" })
-  else
-    vim.o.showtabline = 2
-    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Tabufline" })
-  end
+	if vim.o.showtabline == 2 then
+		vim.o.showtabline = 0
+		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Tabufline" })
+	else
+		vim.o.showtabline = 2
+		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Tabufline" })
+	end
 end, { desc = "Options | Toggle Tabufline", silent = true })
 
 -- Toggle Statusline
 vim.keymap.set("n", "<leader>os", function()
-  if vim.o.laststatus == 3 then
-    vim.o.laststatus = 0
-    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Statusline" })
-  else
-    vim.o.laststatus = 3
-    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Statusline" })
-  end
+	if vim.o.laststatus == 3 then
+		vim.o.laststatus = 0
+		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Statusline" })
+	else
+		vim.o.laststatus = 3
+		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Statusline" })
+	end
 end, { desc = "Options | Toggle Statusline", silent = true })
 
 -- Toggle Line Number
 vim.keymap.set("n", "<leader>ol", function()
-  vim.o.number = not vim.o.number
-  if vim.o.number then
-    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Line Number" })
-  else
-    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Line Number" })
-  end
+	vim.o.number = not vim.o.number
+	if vim.o.number then
+		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Line Number" })
+	else
+		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Line Number" })
+	end
 end, { desc = "Options | Toggle Line Number", silent = true })
 
 -- Toggle Relative Number
 vim.keymap.set("n", "<leader>or", function()
-  vim.o.relativenumber = not vim.o.relativenumber
-  if vim.o.relativenumber then
-    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Relative Number" })
-  else
-    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Relative Number" })
-  end
+	vim.o.relativenumber = not vim.o.relativenumber
+	if vim.o.relativenumber then
+		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Relative Number" })
+	else
+		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Relative Number" })
+	end
 end, { desc = "Options | Toggle Relative Number", silent = true })
 
 -- Toggle Spell Check
 vim.keymap.set("n", "<leader>oS", function()
-  vim.wo.spell = not vim.wo.spell
-  if vim.wo.spell then
-    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Spell Check" })
-  else
-    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Spell Check" })
-  end
+	vim.wo.spell = not vim.wo.spell
+	if vim.wo.spell then
+		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Spell Check" })
+	else
+		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Spell Check" })
+	end
 end, { desc = "Options | Toggle Spell Check", silent = true })
-
--- Toggle Theme
--- vim.keymap.set("n", "<leader>ot", function()
---   local opts = require("nvconfig").base46
---   require("base46").toggle_theme()
---   if opts.theme_toggle == nil or (opts.theme ~= opts.theme_toggle[1] and opts.theme ~= opts.theme_toggle[2]) then
---     return
---   end
---   if opts.theme == opts.theme_toggle[1] then
---     vim.notify("Dark Mode", vim.log.levels.INFO, { title = "Theme" })
---   else
---     vim.notify("Light Mode", vim.log.levels.INFO, { title = "Theme" })
---   end
--- end, { desc = "Options | Toggle Theme", silent = true })
-
--- Toggle Transparency
--- vim.keymap.set("n", "<leader>oT", function()
---   require("base46").toggle_transparency()
---   if require("nvconfig").base46.transparency then
---     vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#000000" })
---     vim.notify("Toggled On", vim.log.levels.INFO, { title = "Transparency" })
---   else
---     vim.api.nvim_set_hl(0, "NotifyBackground", { link = "Normal" })
---     vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Transparency" })
---   end
--- end, { desc = "Options | Toggle Transparency", silent = true })
 
 -- Toggle Wrap
 vim.keymap.set("n", "<leader>ow", function()
-  vim.wo.wrap = not vim.wo.wrap
-  if vim.wo.wrap then
-    vim.notify("Toggled On", vim.log.levels.INFO, { title = "Line Wrap" })
-  else
-    vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Line Wrap" })
-  end
+	vim.wo.wrap = not vim.wo.wrap
+	if vim.wo.wrap then
+		vim.notify("Toggled On", vim.log.levels.INFO, { title = "Line Wrap" })
+	else
+		vim.notify("Toggled Off", vim.log.levels.INFO, { title = "Line Wrap" })
+	end
 end, { desc = "Options | Toggle Wrap", silent = true })
 
 -- Better Down
@@ -162,14 +133,14 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "General | Go to right window", 
 
 -- Close window
 vim.keymap.set("n", "<leader>w", function()
-  if vim.bo.buftype == "terminal" then
-    vim.cmd "Bdelete!"
-    vim.cmd "silent! close"
-  elseif #vim.api.nvim_list_wins() > 1 then
-    vim.cmd "silent! close"
-  else
-    vim.notify("Can't Close Window", vim.log.levels.WARN, { title = "Close Window" })
-  end
+	if vim.bo.buftype == "terminal" then
+		vim.cmd("Bdelete!")
+		vim.cmd("silent! close")
+	elseif #vim.api.nvim_list_wins() > 1 then
+		vim.cmd("silent! close")
+	else
+		vim.notify("Can't Close Window", vim.log.levels.WARN, { title = "Close Window" })
+	end
 end, { desc = "General | Close window", silent = true })
 
 -- Add size at the top
@@ -180,29 +151,19 @@ vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>", { desc = "General | Add si
 
 -- Add size at the left
 vim.keymap.set(
-  "n",
-  "<C-Right>",
-  "<cmd>vertical resize +2<CR>",
-  { desc = "General | Add size at the left", silent = true }
+	"n",
+	"<C-Right>",
+	"<cmd>vertical resize +2<CR>",
+	{ desc = "General | Add size at the left", silent = true }
 )
 
 -- Add size at the right
 vim.keymap.set(
-  "n",
-  "<C-Left>",
-  "<cmd>vertical resize -2<CR>",
-  { desc = "General | Add size at the right", silent = true }
+	"n",
+	"<C-Left>",
+	"<cmd>vertical resize -2<CR>",
+	{ desc = "General | Add size at the right", silent = true }
 )
-
--- Go to previous buffer
-vim.keymap.set("n", "H", function()
-  require("nvchad.tabufline").prev()
-end, { desc = "General | Go to previous buffer", silent = true })
-
--- Go to next buffer
-vim.keymap.set("n", "L", function()
-  require("nvchad.tabufline").next()
-end, { desc = "General | Go to next buffer", silent = true })
 
 -- Go to previous tab
 vim.keymap.set("n", "<Left>", "<cmd>tabprevious<CR>", { desc = "General | Go to previous tab", silent = true })
@@ -265,61 +226,34 @@ vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-j>", { desc = "General | Go to upper
 vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-k>", { desc = "General | Go to lower window(Terminal)", silent = true })
 
 -- Go to left window (Terminal)
-vim.keymap.set("t", "<C-h>", "<C-\\<C-N><C-h>", {  desc = "General | Go to left window(Terminal)", silent = true })
+vim.keymap.set("t", "<C-h>", "<C-\\<C-N><C-h>", { desc = "General | Go to left window(Terminal)", silent = true })
 
 -- Go to right window (Terminal)
 vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-l>", { desc = "General | Go to right window(Terminal)", silent = true })
 
 -- Word Search Increment
 vim.keymap.set("c", "<Tab>", function()
-  if fn.getcmdtype() == "/" or fn.getcmdtype() == "?" then
-    return "<CR>/<C-r>/"
-  end
-  return "<C-z>"
+	if fn.getcmdtype() == "/" or fn.getcmdtype() == "?" then
+		return "<CR>/<C-r>/"
+	end
+	return "<C-z>"
 end, { desc = "General | Word Search Increment", expr = true })
 
 -- Word Search Decrement
 vim.keymap.set("c", "<S-Tab>", function()
-  if fn.getcmdtype() == "/" or fn.getcmdtype() == "?" then
-    return "<CR>?<C-r>/"
-  end
-  return "<S-Tab>"
+	if fn.getcmdtype() == "/" or fn.getcmdtype() == "?" then
+		return "<CR>?<C-r>/"
+	end
+	return "<S-Tab>"
 end, { desc = "General | Word Search Decrement", expr = true })
-
--- Find Config Files
--- vim.keymap.set("n", "<leader>nf", function()
---   require("telescope.builtin").find_files {
---     prompt_title = "Config Files",
---     search_dirs = config_dir,
---     cwd = cwd,
---   }
--- end, { desc = "Neovim | Find Config Files", silent = true })
-
--- Grep Config Files
--- vim.keymap.set("n", "<leader>ng", function()
---   require("telescope.builtin").live_grep {
---     prompt_title = "Config Files",
---     search_dirs = config_dir,
---     cwd = cwd,
---   }
--- end, { desc = "Neovim | Grep Config Files", silent = true })
-
--- Toggle Cheatsheet
--- vim.keymap.set("n", "<leader>nc", function()
---   if vim.bo.filetype == "nvcheatsheet" then
---     vim.cmd "silent! close"
---   else
---     vim.cmd "NvCheatsheet"
---   end
--- end, { desc = "Neovim | Toggle Cheatsheet", silent = true })
 
 -- Inspect
 vim.keymap.set("n", "<leader>ni", function()
-  if vim.version().minor >= 9 then
-    vim.cmd "Inspect"
-  else
-    vim.notify("Inspect isn't available in this neovim version", vim.log.levels.WARN, { title = "Inspect" })
-  end
+	if vim.version().minor >= 9 then
+		vim.cmd("Inspect")
+	else
+		vim.notify("Inspect isn't available in this neovim version", vim.log.levels.WARN, { title = "Inspect" })
+	end
 end, { desc = "Neovim | Inspect", silent = true }) -- only available on neovim >= 0.9
 
 -- Messages
@@ -330,14 +264,9 @@ vim.keymap.set("n", "<leader>nh", "<cmd>checkhealth<cr>", { desc = "Neovim | Hea
 
 -- Version
 vim.keymap.set("n", "<leader>nv", function()
-  local version = vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
-  return vim.notify(version, vim.log.levels.INFO, { title = "Neovim Version" })
+	local version = vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch
+	return vim.notify(version, vim.log.levels.INFO, { title = "Neovim Version" })
 end, { desc = "Neovim | Version", silent = true })
-
--- Run Code
-vim.keymap.set("n", "<leader>nr", function()
-  RunCode()
-end, { desc = "Neovim | Run Code", silent = true })
 
 -- Clean
 vim.keymap.set("n", "<leader>pc", "<cmd>Lazy clean<cr>", { desc = "Lazy | Clean", silent = true })
--- lua/themellowpea/plugins/which-key.lua
+++ lua/themellowpea/plugins/which-key.lua
@@ -1,44 +1,43 @@
 -- NOTE: Keymaps Popup/Guide
 local M = {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
+	"folke/which-key.nvim",
+	event = "VeryLazy",
 }
 
+local icons = require("themellowpea.utils.icons")
 M.opts = {
-    icons = {
-      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-      separator = "➜", -- symbol used between a key and it's label
-      group = "", -- symbol prepended to a group
-    },
-    preset = "modern",
-    win = {
-      border = vim.g.border_enabled and "rounded" or "none",
-    },
-    delay = function()
-      return 0
-    end,
+	icons = {
+		breadcrumb = icons.ui.DividerRight, -- symbol used in the command line area that shows your active key combo
+		separator = icons.ui.ChevronShortRight, -- symbol used between a key and it's label
+		group = "", -- symbol prepended to a group
+	},
+	preset = "modern",
+	win = {
+		border = vim.g.border_enabled and "rounded" or "none",
+	},
+	delay = function()
+		return 0
+	end,
 }
 
-M.config = function(_, opts) 
-  require("which-key").setup(opts)
-  require("which-key").add {
-    {
-      -- { "<leader>s", group = "Sessions", icon = "󰔚" },
-      { "<leader>T", group = "Tests", icon = "󰙨" },
-      { "<leader>d", group = "Debugging", icon = "" },
-      { "<leader>f", group = "Find", icon = "" },
-      { "<leader>g", group = "Git", icon = "󰊢" },
-      { "<leader>h", group = "Harpoon", icon = "" },
-      { "<leader>l", group = "LSP", icon = "" },
-      { "<leader>n", group = "Neovim", icon = "" },
-      { "<leader>o", group = "Options", icon = "" },
-      { "<leader>p", group = "Plugins", icon = "" },
-      { "<leader>r", group = "Runner", icon = "" },
-      { "<leader>t", group = "Terminal", icon = "" },
-      { "<leader>v", group = "Venv", icon = "" },
-    },
-  }
+M.config = function(_, opts)
+	require("which-key").setup(opts)
+	require("which-key").add({
+		{
+			{ "<leader>e", group = "Nvim Tree", icon = icons.ui.Tree },
+			{ "<leader>s", group = "Window Split", icon = icons.ui.Code },
+			{ "<leader>T", group = "Tests", icon = icons.ui.Scopes },
+			{ "<leader>d", group = "Debugging", icon = icons.ui.Bug },
+			{ "<leader>f", group = "Find", icon = icons.ui.Telescope },
+			{ "<leader>g", group = "Git", icon = icons.git.Branch },
+			{ "<leader>h", group = "Harpoon", icon = icons.ui.Pencil },
+			{ "<leader>l", group = "LSP", icon = icons.ui.Lightbulb },
+			{ "<leader>n", group = "Neovim", icon = icons.ui.NeoVim },
+			{ "<leader>o", group = "Options", icon = icons.ui.Gear },
+			{ "<leader>p", group = "Plugins", icon = icons.ui.Package },
+			{ "<leader>t", group = "Tabs", icon = icons.ui.Code },
+		},
+	})
 end
 
 return M
-
