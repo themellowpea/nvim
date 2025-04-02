d9d77bfcc985febfc0cb9cc20fe9d3e96d216225
Matt <themlp@icloud.com>
2025-04-02 08:45:13 -0400

    add some autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- General Settings
+local general = augroup("General", { clear = true })
+
+autocmd({ "FocusLost", "BufLeave", "BufWinLeave", "InsertLeave" }, {
+  -- nested = true, -- for format on save
+  callback = function()
+    if vim.bo.filetype ~= "" and vim.bo.buftype == "" then
+      vim.cmd("silent! w")
+    end
+  end,
+  group = general,
+  desc = "Auto Save",
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local g = vim.g
+
+-- LazyVim autoformat
+g.autoformat = true
+
+local opt = vim.opt
+
+-- so that `` is visible in markdown files
+opt.conceallevel = 1
