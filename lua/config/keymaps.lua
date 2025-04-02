5a61b43c0ff4e02c6c8bba28c626ac2d97d414f8
Matt <themlp@icloud.com>
2025-04-02 08:36:02 -0400

    add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap
+
+map.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+map.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
+
+-- Remove All Text
+map.set("n", "<leader>R", "<cmd>%d+<cr>", { desc = "General | Remove All Text", silent = true })
+-- Yank All Text
+map.set("n", "<leader>Y", "<cmd>%y+<cr>", { desc = "General | Yank All Text", silent = true })
+-- Quit
+map.set("n", "<leader>qQ", "<cmd>qa!<cr>", { desc = "General | Quit", silent = true })
