75d21ccd9395d3e0bb0514748e2fb96b9cdbc903
Matt <themlp@icloud.com>
2025-04-04 07:58:24 -0400

    fix snippets remove mini snippets

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,120 @@
+-- NOTE: Completions
+
+return {
+
+  {
+    "hrsh7th/nvim-cmp",
+
+    dependencies = {
+      {
+        "hrsh7th/cmp-cmdline",
+        event = "CmdlineEnter",
+      },
+      {
+        "hrsh7th/cmp-nvim-lua",
+        event = "InsertEnter",
+      },
+
+      {
+        "hrsh7th/cmp-emoji",
+        event = "InsertEnter",
+      },
+
+      {
+        "saadparwaiz1/cmp_luasnip",
+        event = "InsertEnter",
+      },
+      {
+        "onsails/lspkind.nvim", -- vs-code like pictograms
+        event = "InsertEnter",
+      },
+    },
+    opts = function(_, opts)
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+      local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
+      local auto_select = true
+
+      opts.snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      }
+      table.insert(opts.sources, { name = "luasnip" })
+
+      return {
+        auto_brackets = {}, -- configure any filetype to auto add brackets
+        completion = {
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+          ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+          ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+          ["<tab>"] = function(fallback)
+            return LazyVim.cmp.map({ "snippet_forward", "ai_accept" }, fallback)()
+          end,
+        }),
+        sources = cmp.config.sources({
+          { name = "lazydev" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "cmp_tabnine" },
+          { name = "nvim_lua" },
+          { name = "render-markdown" },
+          { name = "path" },
+          { name = "emoji" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = function(entry, item)
+            local icons = LazyVim.config.icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+              end
+            end
+
+            return item
+          end,
+        },
+        window = {
+          completion = {
+            border = "rounded",
+            scrollbar = false,
+          },
+          documentation = {
+            border = "rounded",
+          },
+        },
+        experimental = {
+          -- only show ghost text when we show ai completions
+          ghost_text = vim.g.ai_cmp and {
+            hl_group = "CmpGhostText",
+          } or false,
+        },
+        sorting = defaults.sorting,
+      }
+    end,
+  },
+}
