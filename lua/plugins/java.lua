b06c51361f3aae9d30b2ee4186bbdce75e76122d
Matt <themlp@icloud.com>
2025-04-04 15:47:55 -0400

    add nvim-java

---
+++ lua/plugins/java.lua
@@ -0,0 +1,56 @@
+-- NOTE: Java
+
+return {
+  "nvim-java/nvim-java",
+  config = false,
+  dependencies = {
+    {
+      "neovim/nvim-lspconfig",
+      opts = {
+        servers = {
+          -- mason = false, -- set to false if you don't want this server to be installed with mason
+          -- Use this to add any additional keymaps
+          -- for specific lsp servers
+          ---@type LazyKeysSpec[]
+          -- keys = {},
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "JavaSE-21",
+                      path = "/Users/themellowpea/.sdkman/candidates/java/21.0.6-tem",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+
+        setup = {
+          jdtls = function()
+            require("java").setup({
+              verification = {
+                invalid_order = false,
+                duplicate_setup_calls = false,
+                -- invalid_mason_registry = true,
+              },
+              root_markers = {
+                "settings.gradle",
+                "settings.gradle.kts",
+                "pom.xml",
+                "build.gradle",
+                "mvnw",
+                "gradlew",
+                "build.gradle",
+                "build.gradle.kts",
+              },
+            })
+          end,
+        },
+      },
+    },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,23 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-    config = function()
-      local servers = {
-        "bashls",
-        "cssls",
-        "eslint",
-        "gopls",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "pyright",
-        "sqlls",
-        "tailwindcss",
-        "ts_ls",
-      }
-
-      require("mason-lspconfig").setup({
-        ensure_installed = servers,
-      })
-    end,
-  },
+  -- Mason
+  -- add servers here
   {
     "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "yamlfmt",
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "prettier",
-        "prettierd",
-        "goimports",
-        "gofumpt",
-        "flake8",
-        "eslint_d",
-      },
-    },
+    opts = function()
+      return {
+        ensure_installed = {
+          "yamlfmt",
+          "stylua",
+          "shellcheck",
+          "shfmt",
+          "prettier",
+          "prettierd",
+          "goimports",
+          "gofumpt",
+          "flake8",
+          "eslint_d",
+        },
+      }
+    end,
   },
 }
