3ba192be850f5b7b00852d286469aad6d41aa57f
Matt <themlp@icloud.com>
2025-04-05 21:32:48 -0400

    add emoji cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,5 +1,4 @@
 -- NOTE: Completions
-
 return {
   {
     "saghen/blink.cmp",
@@ -10,28 +9,35 @@ return {
         -- make sure to set opts so that lazy.nvim calls blink.compat's setup
         opts = {},
       },
+      {
+        "hrsh7th/cmp-emoji",
+        event = "InsertEnter",
+      },
     },
 
     opts = {
-
       snippets = {
         preset = "luasnip",
       },
-
+      keymap = {},
       sources = {
+        compat = {
+          "emoji",
+        },
         -- add lazydev to your completion providers
-        default = { "lazydev" },
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
         providers = {
+          emoji = {
+            name = "emoji",
+            module = "blink.compat.source",
+            score_offset = -3,
+          },
           lazydev = {
             name = "LazyDev",
             module = "lazydev.integrations.blink",
-            score_offset = 100, -- show at a higher priority than lsp
+            score_offset = -3, -- show at a higher priority than lsp
           },
         },
-      },
-
-      cmdline = {
-        enabled = true,
       },
     },
   },
